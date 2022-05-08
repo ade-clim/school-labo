cbc51535ce36764e26e7b844a735f69a235d8a40
ade-clim <35257508+ade-clim@users.noreply.github.com>
2022-05-08 17:36:45 +0200

    Update ennemy.js

--- ennemy.js
+++ ennemy.js
@@ -11,7 +11,6 @@ function spawnGhosts(){
   ghosts = [];
   
   for (let i = 0; i < 20; i++) {
-    
     const newGhost = getCreateGhost();
     ghosts.push(newGhost);
   }
@@ -20,16 +19,17 @@ function spawnGhosts(){
 // display ghost and bullet and manage collision
 function displayBulletAndEnnemies(){
   for (let bullet of bullets) {
+    if(bullet.y < 0) {
+      bullets.splice(bullets.indexOf(bullet), 1); // delete bullet if outside screen
+    }
     bullet.y -= 10;
     bullet.show();
   }
 
   // update and draw enemies
   for (let ghost of ghosts) {
-    
     ghost.y += 1;
     ghost.show();
-    //image(ghost01,enemy.x, enemy.y,142,182);
     if (ghost.y > height) {
       ghosts.splice(ghosts.indexOf(ghost), 1);
     }
@@ -37,15 +37,14 @@ function displayBulletAndEnnemies(){
 
   // deal with collisions
   for (let ghost of ghosts) {
-    
     for (let bullet of bullets) {
-
+      //if collision betweeb ghost and bullet, remove ghost and bullet
       if (dist((ghost.x + 142/2), (ghost.y + 182/2), bullet.x, bullet.y) < 60) {
-        
         ghosts.splice(ghosts.indexOf(ghost), 1);
-        soundGhostVanish.play(); // play sound if ghost is dead
         bullets.splice(bullets.indexOf(bullet), 1);
+        soundGhostVanish.play(); // play sound if ghost is dead
         
+        //create new ghost
         const newGhost = getCreateGhost();
         ghosts.push(newGhost);
         score++;
